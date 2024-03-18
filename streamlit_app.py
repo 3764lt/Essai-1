074754afe17836803a597711c8ed31097ded41f1
3764lt <162983002+3764lt@users.noreply.github.com>
2024-03-18 12:03:36 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,10 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 voc=pd.read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vQu7A9rsNdCjubSwgVFXnOZH7CBRXV-iUNVnBKB6HSXuIHhFSdRdPexzHftf8Kx3ViL6e0CoQpK6vtp/pub?output=csv")
-st.dataframe(voc)
 l=voc.shape[0]
 i=np.random.choice(range(l))
 word_fr=voc["Définition"].values[i]
 word_chi=voc["Hanzi"].values[i]
 st.write(word_fr+" "+word_chi)
+st.button("refresh")
 
