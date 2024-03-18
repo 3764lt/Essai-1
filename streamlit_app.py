3ea0dc9f4cf83cd41e17e2dfabe747c4888289e5
3764lt <162983002+3764lt@users.noreply.github.com>
2024-03-18 12:48:00 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,10 @@ import pandas as pd
 import numpy as np
 voc=pd.read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vQu7A9rsNdCjubSwgVFXnOZH7CBRXV-iUNVnBKB6HSXuIHhFSdRdPexzHftf8Kx3ViL6e0CoQpK6vtp/pub?output=csv")
 l=voc.shape[0]
-i=np.random.choice(range(l))
-indices=np.ramdom.choice(l,size=4,replace=False)
+indices=np.random.choice(l,size=4,replace=False)
+j=np.random.choice(indices)
+word_fr=voc["Définition"].values[j]
+st.write("Traduis:"+word_fr)
 st.write(indices)
 for i in range (4):
   st.button(voc["Hanzi"].values[indices[i]])
