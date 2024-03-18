4f19afdb5441187bec93066486d08d31638ac2e6
3764lt <162983002+3764lt@users.noreply.github.com>
2024-03-18 12:36:44 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,13 +4,11 @@ import numpy as np
 voc=pd.read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vQu7A9rsNdCjubSwgVFXnOZH7CBRXV-iUNVnBKB6HSXuIHhFSdRdPexzHftf8Kx3ViL6e0CoQpK6vtp/pub?output=csv")
 l=voc.shape[0]
 i=np.random.choice(range(l))
-indices=np.ramdom.choices(range(l),h=4,replace=False)
+indices=np.random.choices(range(l),k=4,replace=False)
 st.write(indices)
 word_fr=voc["Définition"].values[i]
-word_pin=voc["Pinyin"].values[i]
-word_chi=voc["Hanzi"].values[i]
-st.write(word_fr+" "+word_pin+" "+word_chi)
-st.button("refresh")
+st.write(word_fr)
+
 
 
 
