dd638a9ad304e7b49217c7bdd883fdee82a1433f
3764lt <162983002+3764lt@users.noreply.github.com>
2024-03-18 11:38:53 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,5 @@
 import streamlit as st 
-st.write("Hello")
+import pandas as pd
+voc=pd.read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vQu7A9rsNdCjubSwgVFXnOZH7CBRXV-iUNVnBKB6HSXuIHhFSdRdPexzHftf8Kx3ViL6e0CoQpK6vtp/pub?output=csv")
+st.dataframe(voc)
+
