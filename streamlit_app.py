d4bcf185a9f0fb28cac17af844ef24e509a6f4d6
3764lt <162983002+3764lt@users.noreply.github.com>
2024-03-18 12:37:43 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import numpy as np
 voc=pd.read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vQu7A9rsNdCjubSwgVFXnOZH7CBRXV-iUNVnBKB6HSXuIHhFSdRdPexzHftf8Kx3ViL6e0CoQpK6vtp/pub?output=csv")
 l=voc.shape[0]
 i=np.random.choice(range(l))
-indices=np.random.choices(range(l),k=4,replace=False)
+indices=np.random.choice(l,size=4,replace=False)
 st.write(indices)
 word_fr=voc["Définition"].values[i]
 st.write(word_fr)
