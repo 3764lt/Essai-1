d8b3ea099aa770cf206b431765559746f3ee5606
3764lt <162983002+3764lt@users.noreply.github.com>
2024-03-18 12:12:41 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,10 @@ voc=pd.read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vQu7A9rsNdCjubS
 l=voc.shape[0]
 i=np.random.choice(range(l))
 word_fr=voc["Définition"].values[i]
+word_pin=voc["Pinyin"].values[i]
 word_chi=voc["Hanzi"].values[i]
-st.write(word_fr+" "+word_chi)
+st.write(word_fr+" "+word_pin+" "+word_chi)
 st.button("refresh")
+
+
 
