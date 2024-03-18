c8353f55e7ad2862fdf5b8cd88e035498b9fd673
3764lt <162983002+3764lt@users.noreply.github.com>
2024-03-18 11:51:37 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,10 @@
 import streamlit as st 
 import pandas as pd
+import numpy as np
 voc=pd.read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vQu7A9rsNdCjubSwgVFXnOZH7CBRXV-iUNVnBKB6HSXuIHhFSdRdPexzHftf8Kx3ViL6e0CoQpK6vtp/pub?output=csv")
 st.dataframe(voc)
+i=np.random.choice(range(l))
+word_fr=voc["Définition"].value[i]
+word_chi=voc[""].values[i]
+st.write(word_fr+""+word_chi)
 
