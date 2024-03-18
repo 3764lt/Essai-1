feae63856522b8e83d7442c9f9618c637265c347
3764lt <162983002+3764lt@users.noreply.github.com>
2024-03-18 11:54:36 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 voc=pd.read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vQu7A9rsNdCjubSwgVFXnOZH7CBRXV-iUNVnBKB6HSXuIHhFSdRdPexzHftf8Kx3ViL6e0CoQpK6vtp/pub?output=csv")
 st.dataframe(voc)
-l=voc.shape[o]
+l=voc.shape[0]
 i=np.random.choice(range(l))
 word_fr=voc["Définition"].value[i]
 word_chi=voc["hanzi"].values[i]
